a0633a457c205c4361f002d27649a4d6764148bb
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-06-03 11:19:14 +0200

    importing encrypted fields from django_crypto_fields

--- edc_data_manager/models.py
+++ edc_data_manager/models.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 from django.core.urlresolvers import reverse
 from django.db import models
 
-from edc_base.encrypted_fields import EncryptedTextField
+from django_crypto_fields.fields import EncryptedTextField
 from edc_base.model.models import BaseModel
 from edc_constants.constants import CLOSED, OPEN
 from edc_registration.models import RegisteredSubject
